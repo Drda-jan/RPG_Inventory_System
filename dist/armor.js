6febe0aeea73d43020576e6b86b6e784bfeb7afa
Drda Jan - IT2B <drdajan246@student.voskh.cz>
2026-05-14 13:58:58 +0200

    skola2

--- dist/armor.js
+++ dist/armor.js
@@ -1,2 +1,8 @@
 //armor.ts
-// 
+// Třída Brneni – potomek Polozka.
+// "import" načte třídu Polozka z jiného souboru, abychom z ní mohli dědit.
+import { Polozka } from "./polozka";
+// Brneni dědí z Polozka a přidává vlastnosti obrana a typ.
+export class Brneni extends Polozka {
+    constructor(id, nazev, vaha, popis, zakladniCena, rarity, multiplikatorRarity, trvaniEfektu, obrana, typ, rychlost) { }
+}
--- dist/potion.js
+++ dist/potion.js
@@ -1,4 +1,28 @@
 // potions.ts
 // Třída Elixir – potomek Polozka.
 // "import" načte třídu Polozka z jiného souboru, abychom z ní mohli dědit.
-import { Poloz
+import { Polozka } from "./polozka";
+// lektvar dědí z Polozka a přidává efekt a typ. 
+export class lektvar extends Polozka {
+    constructor(id, nazev, vaha, popis, zakladniCena, rarity, multiplikatorRarity, trvaniEfektu, efekt, typ) {
+        super(id, nazev, vaha, popis, zakladniCena, rarity, trvaniEfektu, multiplikatorRarity);
+        if (!efekt || efekt.trim() === "")
+            throw new Error("efekt nesmí být prázdný.");
+        this.efekt = efekt;
+        this.typ = typ;
+    }
+    getEfekt() {
+        return this.efekt;
+    }
+    getTyp() {
+        return this.typ;
+    }
+    //okamžitý lektvar (trvání = 0) má vždy hodnotu 10.
+    //buff lektvar (trvání > 0) má hodnotu trvání/10 - čím déle trvá, tím je efektivnější.
+    vypocitejEfektivitu() {
+        if (this.getTrvaniEfektu() > 0) {
+            return this.getTrvaniEfektu() / 10;
+        }
+        return 10;
+    }
+}
