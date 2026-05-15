b2240f46c73d20ddda9b654b76ebee5d2d5716f5
Drda-Jan <drdajan246@student.voskh.cz>
2026-05-15 17:12:23 +0200

    z domova2

--- src/armor.ts
+++ src/armor.ts
@@ -10,4 +10,30 @@ export class Brneni extends Polozka {
     private readonly typ: string;
     private readonly rychlost: number;
 
-    constructor(id: string, nazev: string, vaha: number, popis: string, zakladniCena: number, rarity: string, multiplikatorRarity: number, trvaniEfektu: number, obrana: number, typ: string, rychlost: number) {
+    constructor(id: string, nazev: string, vaha: number, popis: string, zakladniCena: number, rarity: string, multiplikatorRarity: number, obrana: number, rychlost: number, typ: string) {
+        super(id, nazev, vaha, popis, zakladniCena, rarity, 0, multiplikatorRarity);
+
+        if (obrana<= 0) throw new Error("Obrana musí být kladné číslo.");
+        this.obrana = obrana;
+        this.rychlost = rychlost;
+        this.typ = typ; 
+
+    }
+
+    public getObrana(): number {
+        return this.obrana;
+    }
+    public getRychlost(): number {
+        return this.rychlost;
+    }
+    public getTyp(): string {
+        return this.typ;
+    }
+
+    //Efektivita = obrana/vaha
+    //lehke brneni s vysokou obranou dostane nejlepsi skore
+
+    public vypocitejEfektivitu()  : number {
+        return this.obrana / this.getVaha();
+    }
+}
